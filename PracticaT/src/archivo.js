ebe369ead8dd2f66b7ad01909cde6e5d3546e933
Rodrigo1273 <23090536@alumno.utmetropolitana.edu.mx>
2024-05-31 16:55:52 -0600

    oso

---
+++ PracticaT/src/archivo.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js}', // Incluir todos los archivos en src
+      './index.html',         // Incluir el archivo HTML principal en la raíz del proyecto
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
